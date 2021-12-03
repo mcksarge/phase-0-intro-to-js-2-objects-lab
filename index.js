739046152ab534b9bc6e5c69467c358e48c93735
mcksarge <higgins.fred.s@gmail.com>
2021-12-02 21:04:40 -0800

    completed assignment

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 const employee = {};
 
-function  updateEmployeeWithKeyAndValue(employee, key, value) {
-    const newEmployee = {...employee};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let newEmployee = {...employee};
     newEmployee[key] = value;
-    
+
     return newEmployee;
 };
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     employee[key] = value;
-
     return employee;
 };
 
 function deleteFromEmployeeByKey(employee, key) {
-    const delEmployee = {...employee};
-    
-    delete delEmployee[key];
-
-    return delEmployee;
+    let removeEmployee = {...employee};
+    delete removeEmployee[key];
+    return removeEmployee;
 };
 
-function  destructivelyDeleteFromEmployeeByKey(employee, key) {
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key];
-
     return employee;
-};
+}
+
